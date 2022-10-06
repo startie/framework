1099e1e8794088abd7a6b8412ddd72411428c2ab
Kirill Devope <accounts@devope.ru>
2022-10-06 12:42:07 +0300

    Template debugging and refactoring

--- src/Framework/Template.php
+++ src/Framework/Template.php
@@ -8,18 +8,29 @@ use Startie\Input;
 
 class Template
 {
-    public static function r($template, $data, $csrf = null)
+    public static function r($templatePath, $data, $csrf = null)
     {
-        # Initialize Mustache
+        #
+        #   Initialize Mustache
 
-        $m = new Mustache_Engine(array(
+        $mustache = new Mustache_Engine(array(
             'pragmas' => [Mustache_Engine::PRAGMA_BLOCKS],
             'loader' => new Mustache_Loader_FilesystemLoader(BACKEND_DIR . "Templates/", [
                 'extension' => '.mst'
             ]),
+            'escape' => function ($value) {
+                if (is_array($value)) {
+                    $value = var_export($value, true);
+                    throw new \Startie\Exception(
+                        "Value passed to the template can't be an array: $value"
+                    );
+                }
+                return htmlspecialchars($value, ENT_COMPAT, 'UTF-8');
+            },
         ));
 
-        # Csrf protection
+        #
+        #   Csrf protection
 
         if ($csrf == 'csrf') {
 
@@ -49,13 +60,15 @@ class Template
             }
         }
 
-        # Render
+        #
+        #   Render
 
-        return $m->render($template, $data);
+        $template = $mustache->loadTemplate($templatePath);
+        return $template->render($data);
     }
 
-    public static function render($template, $data, $csrf = null)
+    public static function render($templatePath, $data, $csrf = null)
     {
-        echo self::r($template, $data, $csrf);
+        echo self::r($templatePath, $data, $csrf);
     }
 }
