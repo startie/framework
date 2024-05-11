e8afec819b21f68e203bfa4b79dec3ee1d3cd06d
Kirill <mail@devope.ru>
2024-05-11 15:56:56 +0300

    more helpers

--- src/helpers.php
+++ src/helpers.php
@@ -2,7 +2,29 @@
 
 use Startie\Dump;
 
+function d($result = NULL, $die = 0, $msg = NULL, $trace = NULL)
+{
+    Dump::make($result, $die, $msg, $trace);
+}
+
 function dd($result = NULL, $msg = NULL, $trace = NULL)
 {
     Dump::made($result, $msg, $trace);
+}
+
+function t(string $str = "", string $fallback = "")
+{
+    global $t;
+    $result = "";
+
+    if ($str === "") {
+        $result = "";
+    } else {
+        $result = $t[$str]
+            ?? $t[str_replace(" ", "_", $str)]
+            ?? $fallback
+            ?? "";
+    }
+
+    return $result;
 }
