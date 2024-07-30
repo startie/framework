fadd7ed2032de6f2a5d35f2fc99e0718a7f05d4f
Kirill <mail@devope.ru>
2024-07-30 15:32:22 +0300

    new dump helper, and docs

--- src/helpers.php
+++ src/helpers.php
@@ -2,31 +2,57 @@
 
 use Startie\Dump;
 
+/**
+ * Shortcut for `Dump::make()`
+ */
 function d($result = NULL, $die = 0, $msg = NULL, $trace = NULL)
 {
     Dump::make($result, $die, $msg, $trace);
 }
 
+/**
+ * Shortcut for `Dump::make()`
+ */
+function dump($result = NULL, $die = 0, $msg = NULL, $trace = NULL)
+{
+    Dump::make($result, $die, $msg, $trace);
+}
+
+/**
+ * Shortcut for `Dump::made()`
+ */
 function dd($result = NULL, $msg = NULL, $trace = NULL)
 {
     Dump::made($result, $msg, $trace);
 }
 
+/**
+ * Shortcut for `Texts::translate()`
+ */
 function t(string $str = "", string $fallback = "")
 {
     return \Startie\Texts::translate($str, $fallback);
 }
 
+/**
+ * Shortcut for `View::r()`
+ */
 function view($name, array $data = [], bool $trimSpaces = false)
 {
     return \Startie\View::r($name, $data, $trimSpaces);
 }
 
+/**
+ * Shortcut for `View::r()`
+ */
 function v($name, array $data = [], bool $trimSpaces = false)
 {
     return \Startie\View::r($name, $data, $trimSpaces);
 }
 
+/**
+ * Shortcut for `Url::controller()`
+ */
 function url(
     string $routeExpression,
     $controllerParams = NULL,
@@ -41,6 +67,9 @@ function url(
     );
 }
 
+/**
+ * Shortcut for `Template::return()`
+ */
 function template($templatePath, $data, $csrf = NULL)
 {
     return \Startie\Template::return($templatePath, $data, $csrf);
