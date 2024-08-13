c027f352cce201f63822e2a4c069b0bc65595eb1
Kirill <mail@devope.ru>
2024-08-13 22:00:43 +0300

    In docs and enhancements

--- src/Framework/In.php
+++ src/Framework/In.php
@@ -4,116 +4,139 @@ namespace Startie;
 
 class In
 {
-	/**
-	 * 
-	 * Get a variable value from global array and make optional modifications.
-	 * 
-	 * ```php
-	 * In::e('get', 'query', 'STR', ['', NULL], ['trim']);
-	 * ```
-	 * 
-	 * @param  string $global 			A name of global array: 'get' for $_GET, 'post' for $_POST, etc.
-	 * @param  string $name 			A variable name.
-	 * @param  string $SanitizeType 	Type string for sanitizing.
-	 * @param  array $if 				Array of 3 values: 0 – value for equality check; 1 – true case substitute; 2 – false case substitute
-	 * @param  array $processing		Array of functions to call on value.
-	 * @param  array $replacements		Array of replacements.
-	 * 
-	 * @return mixed
-	 * 
-	 */
-
-	public static function e($global, $name, $SanitizeType, $if = [], $processing = [], $replacements = [])
-	{
-		/*
+    /**
+     * Get a 'variable' from global array and make optional modifications.
+     * 
+     * ```php
+     * In::e('get', 'query', 'STR', ['', NULL], ['trim']);
+     * ```
+     * 
+     * @param string $global A name of global variable: 'get' for $_GET, 'post' for $_POST, etc.
+     * @param string $key A key name.
+     * @param string $sanitizeType Type string for sanitizing.
+     * @param array $if Array of 3 values: 0 – value for equality check; 1 – true case substitute; 2 – false case substitute
+     * @param string[] $processing Functions to call on value.
+     * @param array[] $replacements String replacements.
+     * 
+     * @return mixed
+     */
+    public static function e($global, $key, $sanitizeType, $if = [], $processing = [], $replacements = [])
+    {
+        /*
 			fix name
 		*/
 
-		/*
+        /*
 			evaluate type
 		*/
 
-		if ($SanitizeType === '') {
-			$SanitizeType = Input::$SanitizeTypeDefault;
-		}
+        if ($sanitizeType === '') {
+            $sanitizeType = Input::$SanitizeTypeDefault;
+        }
 
-		$data = Input::$global($name, $SanitizeType);
+        $data = Input::$global($key, $sanitizeType);
 
-		/* if */
+        /* if */
 
-		if (!empty($if)) {
-			if ($data === $if[0]) {
-				$data = $if[1];
-			} else {
-				if (!empty($if[2])) {
-					$data = $if[2];
-				} else {
-					//$data = $data;
-				}
-			}
-		}
+        if (!empty($if)) {
+            $conditionValue = $if[0];
+            $trueCaseValue = $if[1];
+            $falseCaseValue = $if[2] ?? null;
+            if ($data === $conditionValue) {
+                $data = $trueCaseValue;
+            } else {
+                if (!is_null($falseCaseValue)) {
+                    $data = $falseCaseValue;
+                }
+            }
+        }
 
-		/* processing */
+        /* processing */
 
-		if (!empty($processing) && $data) {
-			foreach ($processing as $f) {
-				$data = call_user_func($f, $data);
-			}
-		}
+        if (!empty($processing) && $data) {
+            foreach ($processing as $f) {
+                $data = call_user_func($f, $data);
+            }
+        }
 
-		/* replacements */
+        /* replacements */
 
-		if (!empty($replacements)) {
-			foreach ($replacements as $replacement) {
-				if (isset($replacement[0]) && isset($replacement[1])) {
-					preg_replace(
-						"/" . preg_quote($replacement[0]) . "/",
-						$replacement[1],
-						$data ?? []
-					);
-				}
-			}
-		}
+        if (!empty($replacements)) {
+            foreach ($replacements as $replacement) {
+                if (isset($replacement[0]) && isset($replacement[1])) {
+                    preg_replace(
+                        "/" . preg_quote($replacement[0]) . "/",
+                        $replacement[1],
+                        $data ?? []
+                    );
+                }
+            }
+        }
 
-		return $data;
-	}
-	public static function cookie($name, $SanitizeType = '', $if = [], $processing = [], $replacements = [])
-	{
-		return In::e("cookie", $name, $SanitizeType, $if, $processing, $replacements);
-	}
+        return $data;
+    }
 
-	public static function env($name, $SanitizeType = '', $if = [], $processing = [], $replacements = [])
-	{
-		return In::e("env", $name, $SanitizeType, $if, $processing, $replacements);
-	}
+    /**
+     * Short-hand method
+     */
+    public static function cookie($key, $sanitizeType = '', $if = [], $processing = [], $replacements = [])
+    {
+        return In::e("cookie", $key, $sanitizeType, $if, $processing, $replacements);
+    }
 
-	public static function files($name, $SanitizeType = '', $if = [], $processing = [], $replacements = [])
-	{
-		return In::e("files", $name, $SanitizeType, $if, $processing, $replacements);
-	}
+    /**
+     * Short-hand method
+     */
+    public static function env($key, $sanitizeType = '', $if = [], $processing = [], $replacements = [])
+    {
+        return In::e("env", $key, $sanitizeType, $if, $processing, $replacements);
+    }
 
-	public static function get($name, $SanitizeType = '', $if = [], $processing = [], $replacements = [])
-	{
-		return In::e("get", $name, $SanitizeType, $if, $processing, $replacements);
-	}
+    /**
+     * Short-hand method
+     */
+    public static function files($key, $sanitizeType = '', $if = [], $processing = [], $replacements = [])
+    {
+        return In::e("files", $key, $sanitizeType, $if, $processing, $replacements);
+    }
 
-	public static function post($name, $SanitizeType = '', $if = [], $processing = [], $replacements = [])
-	{
-		return In::e("post", $name, $SanitizeType, $if, $processing, $replacements);
-	}
+    /**
+     * Short-hand method
+     */
+    public static function get($key, $sanitizeType = '', $if = [], $processing = [], $replacements = [])
+    {
+        return In::e("get", $key, $sanitizeType, $if, $processing, $replacements);
+    }
 
-	public static function request($name, $SanitizeType = '', $if = [], $processing = [], $replacements = [])
-	{
-		return In::e("request", $name, $SanitizeType, $if, $processing, $replacements);
-	}
+    /**
+     * Short-hand method
+     */
+    public static function post($key, $sanitizeType = '', $if = [], $processing = [], $replacements = [])
+    {
+        return In::e("post", $key, $sanitizeType, $if, $processing, $replacements);
+    }
 
-	public static function server($name, $SanitizeType = '', $if = [], $processing = [], $replacements = [])
-	{
-		return In::e("server", $name, $SanitizeType, $if, $processing, $replacements);
-	}
+    /**
+     * Short-hand method
+     */
+    public static function request($key, $sanitizeType = '', $if = [], $processing = [], $replacements = [])
+    {
+        return In::e("request", $key, $sanitizeType, $if, $processing, $replacements);
+    }
 
-	public static function session($name, $SanitizeType = '', $if = [], $processing = [], $replacements = [])
-	{
-		return In::e("session", $name, $SanitizeType, $if, $processing, $replacements);
-	}
+    /**
+     * Short-hand method
+     */
+    public static function server($key, $sanitizeType = '', $if = [], $processing = [], $replacements = [])
+    {
+        return In::e("server", $key, $sanitizeType, $if, $processing, $replacements);
+    }
+
+    /**
+     * Short-hand method
+     */
+    public static function session($key, $sanitizeType = '', $if = [], $processing = [], $replacements = [])
+    {
+        return In::e("session", $key, $sanitizeType, $if, $processing, $replacements);
+    }
 }
