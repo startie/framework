edc2a93b4ac3b22e6ebf4cea4c31784ba233369f
Kirill <mail@devope.ru>
2024-08-21 16:41:15 +0300

    enhanced class organization, new method for error

--- src/Framework/Output.php
+++ src/Framework/Output.php
@@ -4,6 +4,23 @@ namespace Startie;
 
 class Output
 {
+    use \Startie\Bootable;
+
+    public static function boot()
+    {
+        self::$isBooted = true;
+        self::loadConfig();
+    }
+
+    public static function loadConfig()
+    {
+        try {
+            self::$config = \Startie\Config::get("Output");
+        } catch (\Exception $e) {
+            
+        }
+    }
+
     public static function json($data): string
     {
         return json_encode($data);
@@ -12,5 +29,25 @@ class Output
     public static function plain(string $data): string
     {
         return $data;
+    }
+
+    /**
+     * Send header with an HTTP error
+     */
+    public static function error(int $code): void
+    {
+        // Can work without boot, it will just show HTTP status
+        // self::requireBoot();
+
+        if (isset(self::$config['error'])) {
+            $errorConfig = self::$config['error'][$code];
+
+            if (isset($errorConfig['view'])) {
+                echo view($errorConfig['view']);
+            }
+        } else {
+            header("HTTP/1.0 $code");
+            die();
+        }
     }
 }
